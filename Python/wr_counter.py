6bb41b8f267dc2af01229ad09261fbf9cc911140
Slevin Forbes <nivelsorbef@gmail.com>
2016-09-05 23:25:00 +0300

    updated wr_counter.py

--- Python/wr_counter.py
+++ Python/wr_counter.py
@@ -1,10 +1,9 @@
 from tkinter import *
-import pickle
 
 root = Tk()
 root.title('Winrate Counter')
-root.minsize(width=140, height=100)
-root.maxsize(width=140, height=100)
+root.minsize(width=160, height=140)
+root.maxsize(width=160, height=140)
 
 value = IntVar()
 value.set(0)
@@ -31,9 +30,24 @@ def decrease_value(*args):
     calc = int((value.get() / (value.get() + value2.get()) * 100))
     winr.set(calc)
 
+def reset_value(*args):
+    value.set(0)
+    value2.set(0)
+    calc = int(0)
+    winr.set(calc)
+
+def save_value(*args):
+    pass
+
+def quit():
+    root.quit()
+
 #Buttons
-winrateButton = Button(root, text="Won ↑", command = increase_value, bg='green', fg='white')
-loseButton = Button(root, text="Lost ↓", command=decrease_value ,bg='red', fg='white')
+winrateButton = Button(root, text=" Won\t↑", command = increase_value, bg='green', fg='white')
+loseButton = Button(root, text=" Lost\t↓", command=decrease_value ,bg='red', fg='white')
+resetButton = Button(root, text="Reset\tØ", command=reset_value, bg='blue', fg='white')
+exitButton = Button(root, text="Quit\t✗", command=quit, bg='firebrick', fg='white')
+# saveButton = Button(root, text="Save ✓ ", command=save_value, bg='purple', fg='white')
 
 #Labels
 valueLabel = Label(root, textvariable=value)
@@ -52,5 +66,10 @@ wrCountLabel.grid(row=3, column=1)
 winrateLabel.grid(row=3, column=2)
 loseButton.grid(row=4, column=2)
 winrateButton.grid(row=4, column=1)
+resetButton.grid(row=5, column=1)
+exitButton.grid(row=5, column=2)
+# saveButton.grid(row=5, column=1)
 
 root.mainloop()
+#{scrpn44+self.qweqeqeweqewq}
+#102GB
