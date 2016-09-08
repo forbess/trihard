3cc55dc08ddd055c425ede513c29178ca7e30e22
Slevin Forbes <nivelsorbef@gmail.com>
2016-09-08 23:49:48 +0300

    added diceRoller.py

---
+++ Python/diceRoller.py
@@ -0,0 +1,22 @@
+import random
+
+class DiceRolling:
+	x = 1
+	def __init__(self):
+		print("Welcome to dice rolling program.\n")
+	def rollDice(self):
+		self.x = 1
+		while True:
+				try:
+					while self.x == 1:
+						print("STATUS: Rolling dice...")
+						self.randNumber = random.randint(1,6)
+						print("STATUS: You rolled '{0}'".format(self.randNumber))
+						input(" --> Press enter to re-roll.\n")
+				except:
+					print("\nERROR.RESTARTING...\n")
+
+diceRoll = DiceRolling()
+
+if __name__ == '__main__':
+	diceRoll.rollDice()
